a661e30f2f2e1536fc357655702be298c1644cf7
sureshgolconda <sureshgolconda@gmail.com>
2014-09-08 23:06:47 -0500

    adding some gprof analysis output. and script for running leve-5 problem

---
+++ scripts/level_5_sample_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Authors: Suresh Golconda
+
+# Sample run to generate longest path for an example start-end node combination on a 32x32 grid.
+
+##Below command will run the program to find longest path from node (0,0) to node (31,3).
+time ../bin/knight_long_path -a -s 0,0 -e 31,3
+
